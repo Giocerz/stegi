4da362cddc35c9d3f8f601e481ff3815c680fcab
giocerz <grce2000@gmail.com>
2023-09-26 01:43:32 -0500

    add select arrow in product page

--- src/Components/ProductPage/ProductPage.jsx
+++ src/Components/ProductPage/ProductPage.jsx
@@ -3,35 +3,35 @@ import '../ProductsShowcase/ProductShowcase.css'
 import './ProductPage.css'
 import { useEffect, useState } from "react";
 import { ShopButtons } from "../Buttons/ShopButtons";
-import { MdStar, MdStarHalf, MdStarBorder } from "react-icons/md";
+import { MdStar, MdStarHalf, MdStarBorder, MdArrowDownward} from "react-icons/md";
 
 const ReviewBox = ({ rate, numberReviews }) => {
     const [stars, setStars] = useState([]);
 
     useEffect(() => {
         let typeStars = [
-            <MdStarBorder key='rev-star-1'/>,
-            <MdStarBorder key='rev-star-2'/>,
-            <MdStarBorder key='rev-star-3'/>,
-            <MdStarBorder key='rev-star-4'/>,
-            <MdStarBorder key='rev-star-5'/>
+            <MdStarBorder key='rev-star-1' />,
+            <MdStarBorder key='rev-star-2' />,
+            <MdStarBorder key='rev-star-3' />,
+            <MdStarBorder key='rev-star-4' />,
+            <MdStarBorder key='rev-star-5' />
         ];
         const decimal = ((rate * 10) % 10) / 10;
         const entero = rate - decimal;
         let i = 0;
-        for(i; i < entero; i++) {
+        for (i; i < entero; i++) {
             typeStars[i] = <MdStar key={`rev-star-${i + 1}`} />;
         }
-        if(decimal > 0) {
+        if (decimal > 0) {
             typeStars[i] = <MdStarHalf key={`rev-star-${i + 1}`} />;
         }
         setStars(typeStars);
     }, [rate])
-    return(
+    return (
         <section className='reviewsBox'>
-             <span>{rate}</span> 
-             <span className='reviewsBox-stars'>{stars && stars.map((value) => value)}</span>
-             <span>{`(${numberReviews})`}</span>
+            <span>{rate}</span>
+            <span className='reviewsBox-stars'>{stars && stars.map((value) => value)}</span>
+            <span>{`(${numberReviews})`}</span>
         </section>
     )
 }
@@ -39,6 +39,7 @@ const ReviewBox = ({ rate, numberReviews }) => {
 function ProductPage() {
     const params = useParams();
     const [productData, setProductData] = useState();
+    const [rotateSelectArrow, setRotateSelectArrow] = useState('');
 
     useEffect(() => {
         fetch('/src/assets/data.json')
@@ -56,7 +57,7 @@ function ProductPage() {
                 </header>
                 <article className='productPage-info'>
                     <h1 className="productPage-title">{productData?.title}</h1>
-                    <ReviewBox rate={productData?.reviews.rating} numberReviews={productData?.reviews.number}/>
+                    <ReviewBox rate={productData?.reviews.rating} numberReviews={productData?.reviews.number} />
                     <span className='productPage-price'>{`$ ${productData?.price}`}</span>
                     <section className='productPage-ship'>
                         <span>Arrives tomorrow</span>
@@ -65,14 +66,17 @@ function ProductPage() {
                     <p className='productPage-description'>{productData?.description}</p>
                     <section className='productPage-quantity'>
                         <label htmlFor="quantity">Quantity: </label>
-                        <select id="quantity" className='productPage-quantity'>
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                            <option>6</option>
-                        </select>
+                        <div className='productPage-quantity-select'>
+                            <select id="quantity" onFocus={() =>  setRotateSelectArrow('rotate')} onBlur={() => setRotateSelectArrow('')}>
+                                <option value='1'>1</option>
+                                <option value='1'>2</option>
+                                <option value='3'>3</option>
+                                <option value='4'>4</option>
+                                <option value='5'>5</option>
+                                <option value='6'>6</option>
+                            </select>
+                            <div className={`productPage-quantity-select-arrow ${rotateSelectArrow}`}><MdArrowDownward /></div>
+                        </div>
                     </section>
                     <section className='productPageInfo-btnContainer'>
                         <ShopButtons>Buy</ShopButtons>
